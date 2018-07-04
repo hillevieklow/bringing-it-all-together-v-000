0e2bc76492577f084b5a7d3e8fec66129bd48832
Hillevi Eklöw <hillevieklow@gmail.com>
2018-07-04 15:50:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,3 +10,16 @@ class Dog
 
   def self.create_table
     sql = "CREAT TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);"
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs;"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT NTO dogs(name, breed) VALUES (?, ?)"
